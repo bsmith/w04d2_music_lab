95d1f48f78c371967d65e1205c690e9be4432fde
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-08 21:54:52 +0000

    Factorise the artist repository

--- console.py
+++ console.py
@@ -1,4 +1,13 @@
 import repositories.artist_repository as artist_repository
+from models.artist import Artist
+
+print(artist_repository.select_all())
+
+artist = Artist('Arty the Artist')
+artist_repository.save(artist)
+print(artist)
+print(artist_repository.select(artist.id))
+artist_repository.delete(artist.id)
 
 # `python3 -i console.py` has better tab completion!
 #import pdb
--- repositories/artist_repository.py
+++ repositories/artist_repository.py
@@ -1,24 +1,35 @@
 from db.run_sql import run_sql
 from models.artist import Artist
 
+SQL_SELECT_ALL = """SELECT * FROM artists"""
+SQL_INSERT = """INSERT INTO artists (name) VALUES (%s) RETURNING *"""
+SQL_SELECT = """SELECT * FROM artists WHERE id = %s"""
+SQL_DELETE = """DELETE FROM artists WHERE id = %s"""
+SQL_DELETE_ALL = """DELETE FROM artists"""
+SQL_UPDATE = """UPDATE artists SET (name) = (%s) WHERE id = %s"""
+
 def make_artist_from_row(row):
     return Artist(row['name'], row['id'])
 
+# NB omits id!
+def make_row_from_artist(artist):
+    return [artist.name]
+
 def select_all():
-    sql = """SELECT * FROM artists"""
+    sql = SQL_SELECT_ALL
     results = run_sql(sql)
     artists = [make_artist_from_row(row) for row in results]
     return artists
 
 def save(artist):
-    sql = """INSERT INTO artists (name) VALUES (%s) RETURNING *"""
-    values = [artist.name]
+    sql = SQL_INSERT
+    values = make_row_from_artist(artist)
     results = run_sql(sql, values)
     artist.id = results[0]['id']
     return artist
 
 def select(id):
-    sql = """SELECT * FROM artists WHERE id = %s"""
+    sql = SQL_SELECT
     values = [id]
     results = run_sql(sql, values)
     if results:
@@ -27,19 +38,18 @@ def select(id):
 
 # XXX check cursor.rowcount to see if we did DELETE/UPDATE something?
 def delete(id):
-    sql = """DELETE FROM artists WHERE id = %s"""
+    sql = SQL_DELETE
     values = [id]
     run_sql(sql, values, do_fetchall=False)
     return True
 
 def delete_all():
-    sql = """DELETE FROM artists"""
+    sql = SQL_DELETE_ALL
     run_sql(sql, do_fetchall=False)
     return True
 
 def update(artist):
-    sql = """UPDATE artists SET (name) = (%s)
-        WHERE id = %s"""
-    values = [artist.name, artist.id]
+    sql = SQL_UPDATE
+    values = make_row_from_artist(artist) + [artist.id]
     run_sql(sql, values, do_fetchall=False)
     return True
